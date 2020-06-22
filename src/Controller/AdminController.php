b18a57cdf6864e7f9656e876dd60773ee630a2ce
si-gidlnk <simon@dalink.fr>
2020-06-22 11:32:57 +0200

    easyadmin + admincontroller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+
+
+class AdminController extends BaseAdminController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
-    public function index()
+
+    public function createNewUserEntity()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function persistUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::persistEntity($user);
+    }
+
+    public function updateUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::updateEntity($user);
     }
 }
