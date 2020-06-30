77931c94d25c6ca3e3508e4a76feae79f04a2e51
si-gidlnk <simon@dalink.fr>
2020-06-30 23:23:09 +0200

    send messages classroom

--- src/Controller/ClassRoomController.php
+++ src/Controller/ClassRoomController.php
@@ -3,13 +3,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacts;
+use Symfony\Component\Validator\Constraints\DateTime;
+use App\Entity\PrivateMessage;
+use App\Entity\PublicMessage;
 use App\Entity\School;
 use App\Entity\Classroom;
+use App\Form\PublicMessageType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Class ClassRoomController
+ * @package App\Controller
+ * @Route("classroom/")
+ */
 class ClassRoomController extends AbstractController
 {
     /** @var EntityManagerInterface */
@@ -18,6 +29,8 @@ class ClassRoomController extends AbstractController
     private $schoolRepository;
     /** @var \Doctrine\Common\Persistence\ObjectRepository */
     private $classRoomRepository;
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $publicMessageRepository;
 
 
     public function __construct(EntityManagerInterface $entityManager)
@@ -25,6 +38,7 @@ class ClassRoomController extends AbstractController
         $this->entityManager = $entityManager;
         $this->schoolRepository = $entityManager->getRepository(School::class);
         $this->classRoomRepository = $entityManager->getRepository(Classroom::class);
+        $this->publicMessageRepository = $entityManager->getRepository(PublicMessage::class);
     }
     /**
      * @Route("/classroom/{id}", name="classroom_id")
@@ -33,6 +47,40 @@ class ClassRoomController extends AbstractController
      */
     public function classroom(Request $request, $id){
 
-        $classroom = $this->classRoomRepository->find($id);
+        $classroom = $this->classRoomRepository->findOneById($id);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/teacher/{classReceiver}", name="class_message")
+     */
+    public function sendMessage(Request $request, $classReceiver){
+
+
+        $classReceiver = $this->classRoomRepository->findOneById($classReceiver);
+
+
+        $message = new PublicMessage();
+        $date = new \DateTime();
+        $message->setSendAt($date);
+
+        $message->setReceiver($classReceiver);
+        $form = $this->createForm(PublicMessageType::class, $message, array('classroom' => $this->getUser()->getClassroom())) ;
+        $form->handleRequest($request);
+
+        // Check is valid
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->entityManager->persist($message);
+
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Message envoyé');
+            //return $this->redirectToRoute('conversation',array("contact" => $receiver));
+        }
+
+        return $this->render('/public_message/message_Form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Form/PublicMessageType.php
+++ src/Form/PublicMessageType.php
@@ -2,18 +2,33 @@
 
 namespace App\Form;
 
+use App\Repository\ClassRoomRepository;
+use App\Repository\UserRepository;
 use App\Entity\PublicMessage;
+use App\Entity\Classroom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PublicMessageType extends AbstractType
 {
+    private $classRoomRepository;
+    public function __construct(ClassRoomRepository $classRoomRepository)
+    {
+        $this->classRoomRepository = $classRoomRepository;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('content', TextareaType::class)
+            ->add('sender', EntityType::class,
+            ['class' => Classroom::class,
+                'choices' => $options['classroom']
+                ]
+            )
             ->add('submit', SubmitType::class,
                 [
                     'attr' => ['class' => 'form-control btn-primary pull-right'],
@@ -26,6 +41,7 @@ class PublicMessageType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => PublicMessage::class,
+            'classroom' => array()
         ]);
     }
 }
