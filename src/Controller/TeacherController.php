09e5db6d91d3ef57279d618f1393269c8a8e626c
bhija <ayoub.bhija77@gmail.com>
2020-07-01 18:54:04 +0200

    finished

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,18 +2,97 @@
 
 namespace App\Controller;
 
+use App\Entity\Classroom;
+use App\Entity\File;
+use App\Entity\Measure;
+use App\Entity\Publication;
+use App\Form\MeasureFormType;
+use App\Repository\ClassRoomRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TeacherController extends AbstractController
 {
+
+
+    private $formE;
+
     /**
      * @Route("/teacher/new_publication", name="new_publication")
      */
-    public function index()
+    public function add_publication(Request $request, EntityManagerInterface $entityManagerInterface)
     {
+
+
+
+        $publication = new Publication();
+        $form = $this->createFormBuilder($publication)
+            ->add("title", TextType::class)
+            ->add("content", TextareaType::class)
+            ->add("files", FileType::class, [
+                'multiple' => true,
+                'mapped' => false
+            ])
+            ->add("classroom", EntityType::class, [
+                'class' => Classroom::class,
+                'choices' => $this->getUser()->getClassroom(),
+                'mapped' => false,
+                'choice_label' => 'name'
+            ])
+            ->add("measure", MeasureFormType::class)
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $formEvent) {
+
+                $this->formE =  $formEvent->getForm();
+                dump($this->formE);
+                $classroom = $this->formE->get("classroom")->getData();
+                dump($classroom);
+                $clr = $this->getDoctrine()->getRepository(Classroom::class)->find($classroom);
+                $this->formE->getData()->setClassroom($clr);
+                $this->formE->getData()->setDate(new \DateTime());
+                dump($this->formE);
+            })
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $filesystem = new Filesystem();
+            $publicationCreated = $form->getData();
+            dump($publication);
+
+            $files = $form['files']->getData();
+            foreach ($files as $file) {
+                $extension = $file->guessExtension();
+                $newNameFile = $publicationCreated->getTitle() . '-' . uniqid() . '.' . $extension;
+                $fileSize = $file->getClientSize();
+                $file->move($this->getParameter('uploads'), $newNameFile);
+                $filer = new File();
+                $filer->setName($newNameFile);
+                $filer->setMimeType($extension);
+                $filer->setFileSize($fileSize);
+                $entityManagerInterface->persist($filer);
+                $publicationCreated->addFile($filer);
+            }
+
+            $entityManagerInterface->persist($publicationCreated);
+            $entityManagerInterface->flush();
+        }
+
+
         return $this->render('teacher/new_publication.html.twig', [
             'controller_name' => 'TeacherController',
+            'form' => $form->createView()
         ]);
     }
 }
